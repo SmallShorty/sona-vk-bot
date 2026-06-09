834f22e3f8ba5bfbbcf2d5d4d9c5568a0abc0084
smallshorty <lmokrousova51@gmail.com>
2026-06-09 22:34:27 +0300

    feat: updated prexix

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
-'use strict';
+"use strict";
 
 module.exports = {
-    PREFIX: '/',
+  PREFIX: "!",
 
-    // VK peer_id беседы для приёма баг-репортов
-    BUG_REPORT_PEER_ID: 2000000003,
+  // VK peer_id беседы для приёма баг-репортов
+  BUG_REPORT_PEER_ID: 2000000003,
 
-    // Смещение peer_id групповых бесед в VK (peerId - offset = group message id)
-    VK_PEER_OFFSET: 2000000000,
+  // Смещение peer_id групповых бесед в VK (peerId - offset = group message id)
+  VK_PEER_OFFSET: 2000000000,
 
-    DICE_MIN_RANGE: 2,
-    DICE_MAX_RANGE: 100,
-    DICE_DEFAULT_RANGE: 20,
+  DICE_MIN_RANGE: 2,
+  DICE_MAX_RANGE: 100,
+  DICE_DEFAULT_RANGE: 20,
 };
